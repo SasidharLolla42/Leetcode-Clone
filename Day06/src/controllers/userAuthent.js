1a252932a4f32be09bf97fab4d2f2432d7836678
sasidhar <sasidharlolla8@gmail.com>
2025-05-11 12:47:49 +0530

    Creating remaining API's

---
+++ Day06/src/controllers/userAuthent.js
@@ -0,0 +1,105 @@
+const redisClient = require("../config/redis");
+const User = require("../models/user");
+const validate = require("../utils/validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  try {
+    //validate the data
+    validate(req.body);
+    const { firstName, emailId, password } = req.body;
+
+    //Storing the password in hash
+    req.body.password = await bcrypt.hash(password, 10);
+    req.body.role = "user";
+
+    const user = await User.create(req.body);
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId, role: "user" },
+      process.env.JWT_KEY,
+      {
+        expiresIn: 60 * 60,
+      }
+    );
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+
+    res.status(201).send("User Registered successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    if (!emailId) throw new Error("Invalid Credentials");
+    if (!password) throw new Error("Invalid Credentials");
+
+    const user = await User.findOne({ emailId });
+
+    const match = bcrypt.compare(password, user.password);
+
+    if (!match) throw new Error("Invalid Credentials");
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId, role: user.role },
+      process.env.JWT_KEY,
+      {
+        expiresIn: 60 * 60,
+      }
+    );
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+    res.status(200).send("Logged In Successfully");
+  } catch (err) {
+    res.status(401).send("Error: " + err);
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    const { token } = req.cookies;
+    const payload = jwt.decode(token);
+
+    await redisClient.set(`token:${token}`, "Blocked");
+    await redisClient.expireAt(`token:${token}`, payload.exp);
+    //Add the token to redis blockList
+    //clear the cookies
+
+    res.cookie("token", null, { expires: new Date(Date.now()) });
+    res.send("Logged Out Successfully");
+  } catch (err) {
+    res.status(503).send("Error: " + err);
+  }
+};
+
+const adminRegister = async (req, res) => {
+  try {
+    //validate the data
+    validate(req.body);
+    const { firstName, emailId, password } = req.body;
+
+    //Storing the password in hash
+    req.body.password = await bcrypt.hash(password, 10);
+    //req.body.role = "admin";
+
+    const user = await User.create(req.body);
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId, role: user.role },
+      process.env.JWT_KEY,
+      {
+        expiresIn: 60 * 60,
+      }
+    );
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+
+    res.status(201).send("User Registered successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err);
+  }
+};
+
+module.exports = { register, login, logout, adminRegister };
---
+++ Day06/src/controllers/userProblem.js
@@ -0,0 +1,176 @@
+const {
+  getLanguageById,
+  submitBatch,
+  submitToken,
+} = require("../utils/problemUtility");
+const Problem = require("../models/problem");
+
+const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    visibleTestCases,
+    hiddenTestCases,
+    startCode,
+    referenceSolution,
+    problemCreator,
+  } = req.body;
+
+  try {
+    for (const { language, completeCode } of referenceSolution) {
+      //sourcecode
+      //language_id
+      //stdin
+      //expectedOutput
+
+      const languageId = getLanguageById(language);
+
+      //Creating batch submission
+      const submissions = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
+
+      const submitResult = await submitBatch(submissions);
+
+      const resultToken = submitResult.map((value) => value.token);
+
+      const testResult = await submitToken(resultToken);
+
+      for (const test of testResult) {
+        if (test.status_id != 3) {
+          return res.status(400).send("Error Occured");
+        }
+      }
+    }
+
+    //We can store it in DB
+    const userProblem = await Problem.create({
+      ...req.body,
+      problemCreator: req.result._id,
+    });
+
+    res.status(201).send("Problem Saved Successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err);
+  }
+};
+
+const updateProblem = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    visibleTestCases,
+    hiddenTestCases,
+    startCode,
+    referenceSolution,
+    problemCreator,
+  } = req.body;
+  try {
+    if (!id) {
+      return res.status(400).send("Missing Id Field");
+    }
+
+    const DsaProblem = await Problem.findById(id);
+    if (!DsaProblem) {
+      return res.status(404).send("ID is not present in server");
+    }
+
+    for (const { language, completeCode } of referenceSolution) {
+      //sourcecode
+      //language_id
+      //stdin
+      //expectedOutput
+
+      const languageId = getLanguageById(language);
+
+      //Creating batch submission
+      const submissions = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
+
+      const submitResult = await submitBatch(submissions);
+
+      const resultToken = submitResult.map((value) => value.token);
+
+      const testResult = await submitToken(resultToken);
+
+      for (const test of testResult) {
+        if (test.status_id != 3) {
+          return res.status(400).send("Error Occured");
+        }
+      }
+    }
+
+    const newProblem = await Problem.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { runValidators: true, new: true }
+    );
+
+    res.status(200).send(newProblem);
+  } catch (err) {
+    res.status(500).send("Error: " + err);
+  }
+};
+
+const deleteProblem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!id) res.status(400).send("ID is Missing");
+
+    const deletedProblem = await Problem.findByIdAndDelete(id);
+
+    if (!deletedProblem) res.status(404).send("Problem is Missing");
+
+    res.status(200).send("Successfully Deleted Problem");
+  } catch (err) {
+    res.status(500).send("Error: " + err);
+  }
+};
+
+const getProblemById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!id) res.status(400).send("ID is Missing");
+
+    const getProblem = await Problem.findById(id);
+
+    if (!getProblem) res.status(404).send("Problem is Missing");
+
+    res.status(200).send(getProblem);
+  } catch (err) {
+    res.status(500).send("Error: " + err);
+  }
+};
+
+const getAllProblem = async (req, res) => {
+  try {
+    const getProblem = await Problem.find({});
+
+    if (getProblem.length==0) 
+      res.status(404).send("Problem is Missing");
+
+    res.status(200).send(getProblem);
+  } catch (err) {
+    res.status(500).send("Error: " + err);
+  }
+};
+
+module.exports = {
+  createProblem,
+  updateProblem,
+  deleteProblem,
+  getProblemById,
+  getAllProblem,
+};
